07e0e5066fb9947d5a1dfc95a7a2166ceed6ed4c
Kevin Miles <kevin.miles@dws.la>
2016-12-08 06:34:45 -0600

    add 5

--- src/2016/5/ab.js
+++ src/2016/5/ab.js
@@ -1,19 +1,53 @@
 import crypto from 'crypto';
 
-let password = '';
-let input = 'uqwqemis';
-let hashInput;
-let index = 0;
-let hash = '';
+const password1 = (input) => {
+  let password = '';
+  let hashInput;
+  let index = 0;
+  let hash = '';
 
-while(password.length !== 8){
-  hashInput = `${input}${index}`;
-  hash = crypto
-  .createHash("md5")
-  .update(hashInput)
-  .digest("hex");
-  if(hash.slice(0,5) === '00000'){ password += hash.slice(5,6);}
-  index++;
+  while(password.length !== 8){
+    hashInput = `${input}${index}`;
+    hash = crypto
+    .createHash("md5")
+    .update(hashInput)
+    .digest("hex");
+    if(hash.slice(0,5) === '00000'){ password += hash.slice(5,6);}
+    index++;
+  }
+
+  return password;
 }
 
-console.log(password);
+const password2 = (input) => {
+  let password = [];
+  let hashInput;
+  let index = 0;
+  let hash = '';
+  let pos;
+
+  while(password.length !== 8){
+    hashInput = `${input}${index}`;
+    hash = crypto
+    .createHash("md5")
+    .update(hashInput)
+    .digest("hex");
+
+    pos = hash.slice(5,6);
+
+    if(hash.slice(0,5) === '00000' && pos >= 0 && pos <= 7){
+      if(!password[pos]) {
+        password[pos] = hash.slice(6,7);
+      }
+
+    }
+    index++;
+  }
+
+  return password.join('');
+}
+
+const input = 'uqwqemis';
+
+console.log('part A:', password1(input));
+console.log('part B:', password2(input));
